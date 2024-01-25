298ebffc4b9609efd19b84a83eabe90112adfb6d
jesieldotdev <jesiel364@gmail.com>
2024-01-24 23:40:40 -0400

    socketio

--- src/assets/pages/Game/viewController.tsx
+++ src/assets/pages/Game/viewController.tsx
@@ -1,17 +1,43 @@
+import React from "react";
 import { useState } from "react"
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001'); // Substitua pela URL do seu servidor WebSocket
+
+interface Player {
+  id: string;
+  name: string;
+}
 
 interface newGameControler{
   
 }
 
 const newGameController = () => {
-    const [open, setOpen] = useState()
+    const [players, setPlayers] = React.useState<Player[]>([]);
+    const [open, setOpen] = React.useState(false)
 
 
     function closeModal(){
         setOpen(false)
     }
 
+    React.useEffect(() => {
+        // Conectar ao servidor WebSocket quando o componente montar
+        socket.connect();
+    
+        // Escutar por atualizações de jogadores
+        socket.on('updatePlayers', (updatedPlayers: Player[]) => {
+          setPlayers(updatedPlayers);
+        });
+    
+        // Cleanup ao desmontar o componente
+        return () => {
+          socket.disconnect();
+        };
+      }, []); 
+
+      console.log(players)
     return{
         open,
         setOpen,
